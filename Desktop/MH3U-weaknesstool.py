3f7b2aacdba9f3973995895f4dba7ca438ff0c07
Tobias Coon <mistertobymon@gmail.com>
2016-01-23 20:00:52 -0500

    second commit

---
+++ Desktop/MH3U-weaknesstool.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+
+loop = 1
+while (loop < 100):   
+  monster = str(input("\n \n Monster Input: "))
+  print("Looking up", monster,". Weaknesses are up higher the stronger they are.")
+
+  if monster in ['Great Jaggi', 'jaggi', 'great jaggi', 'Jaggi']:
+    print("Great Jaggi is weak to: \n Fire \n Water \n Thunder \n and Ice.")
+  if monster in ['Great Baggi', 'baggi', 'great baggi', 'Baggi']:
+    print("Great Baggi is weak to: \n Fire \n Water \n and Thunder.")  
+  if monster in ['Great Wroggi', 'Wroggi', 'great wroggi', 'wroggi']:
+    print("Great Wroggi is weak to: \n Ice \n and Water.")
+  if monster in ['Qurupeco', 'Qurupeco', 'quropeco', 'qurupeco', 'bird']:
+    print("Qurupeco is weak to: \n Ice \n and Water.")
+  if monster in ['Crimson Qurupeco', 'Crimson Qurupeco', 'crimson quropeco', 'crimson qurupeco', 'red bird']:
+    print("Crimson Qurupeco is weak to: \n Ice \n and Fire.")
+  if monster in ['Gigginox', 'giggi', 'giggity', 'gigginox']:
+    print("Gigginox is weak to: \n Fire \n and Dragon.")
+  if monster in ['Baleful Gigginox' 'baleful giggi' 'bale giggity' 'baleful gigginox', 'Christian Bale']:
+    print("Baleful Gigginox is weak to: \n Water \n and Dragon.")
+  if monster in ['Rathian', 'rathian', 'green rath', 'green queen', 'i hate this guy']:
+    print ("Rathian is weak to: \n Dragon \n and Thunder.")
+  if monster in ['Pink Rathian', 'pink rathian', 'pink rath', 'pink queen', 'I HATE POISON']:
+    print ("Pink Rathian is weak to: \n Dragon \n and Thunder.")  
+  if monster in ['Gold Rathian', 'Golden Rathian', 'gold rathian', 'golden rathian', 'gold rath', 'shiny rathian', 'gold']:
+    print ("Gold Rathian is weak to: \n Thunder \n and Water.")  
+  if monster in ['Rathalos', 'NO PLATES', 'rathalos', 'red robin', 'worst monster']:
+    print ("Rathalos is weak to: \n Good Game Design \n Dragon \n and Thunder.")
+  if monster in ['Azure Rathalos', 'azure rathalos', 'azure rath', 'blue rathalos']:
+    print ("Azure Rathalos is weak to: \n Dragon \n and Ice.")  
+  if monster in ['Silver Rathalos', 'silver rath', 'silver rathalos', 'Silver Rath', 'FUCK FIRE']:
+    print ("Silver Rathalos is weak to: \n Thunder \n and Water.")  
+  if monster in ['Nargacuga', 'nargacuga', 'narga', 'best monster']:
+    print ("Nargacuga is weak to: \n Thunder \n and Fire.")  
+  if monster in ['Green Nargacuga', 'green nargacuga', 'green narga', 'goblin']:
+    print ("Green Nargacuga is weak to: \n Thunder \n and Fire.")  
+  if monster in ['Lucent Nargacuga', 'lucent nargacuga', 'lucent narga', 'lucent']:
+    print ("Lucent Nargacuga is weak to: \n Ice \n and Dragon.")  
+  if monster in ['Barioth', 'barry', 'barioth', 'snow tiger']:
+    print ("Barioth is weak to: \n Fire \n and Thunder.")    
+  if monster in ['Sand Barioth', 'sand barry', 'sand barioth', 'desert barioth']:
+    print ("Sand Barioth is weak to: \n Ice \n and Thunder.")    
+  if monster in ['Diablos', 'diablos', 'double monoblos', 'el diablo', 'El Diablo']:
+    print ("Diablos is weak to: \n Ice \n Water \n Thunder \n and Dragon.")    
+  if monster in ['Black Diablos', 'REALLY FAST', 'black diablos', 'black horny']:
+    print ("Black Diablos is weak to: \n Ice \n Water \n Thunder \n and Dragon.")    
+  if monster in ['Plesioth', 'plesioth', 'plessy', 'fish']:
+    print ("Plesioth is weak to: \n Thunder \n and Fire.")    
+  if monster in ['Green Plesioth', 'green plesioth', 'green plessy', 'green fish']:
+    print ("Green Plesioth is weak to: \n Thunder \n and Fire.")    
+  if monster in ['Royal Ludroth', 'royal ludroth', 'ludroth']:
+    print ("Royal Ludroth is weak to: \n Fire \n and Thunder.")   
+  if monster in ['Purple Ludroth', 'purple ludroth']:
+    print ("Purple Ludroth is weak to: \n Fire \n and Thunder.")     
+  if monster in ['Gobul', 'gobul', 'anglerfish']:
+    print ("Gobul is weak to: \n Thunder \n and Fire.")     
+  if monster in ['Nibelsnarf', 'nibelsnarf', 'nibel', 'big guy', 'nibbly']:
+    print ("Nibelsnarf is weak to: \n Ice \n and Thunder.") 
+  if monster in ['Agnaktor', 'agnaktor', 'aggy']:
+    print("Agnaktor is weak to: \n Water \n Thunder \n Ice \n and Dragon.")
+  if monster in ['Glacial Agnaktor', 'Ice Agnaktor', 'glacial agnaktor', 'glacial']:
+    print("Glacial Agnaktor is weak to: \n Fire/Dragon \n and Thunder." )  
+  if monster in ['Lagiacrus', 'terror of land and sea', 'lagiacrus', 'flagship monster #1']:
+    print ("Lagiacrus is weak to: \n Fire \n Ice \n and Dragon.")     
+  if monster in ['Ivory Lagiacrus', 'ivory lagiacrus', 'ivory lagi', 'white lagiacrus']:
+    print ("Ivory Lagiacrus is weak to: \n Fire \n Dragon \n and Ice.")     
+  if monster in ['Abyssal Lagiacrus', 'abyssal lagiacrus', 'abyssal lagi']:
+    print ("Abyssal Lagiacrus is weak to: \n Dragon \n and Fire.")     
+  if monster in ['Barroth', 'barroth', 'b']:
+    print ("Barroth is weak to: \n Fire/Water \n and Ice.")     
+  if monster in ['Jade Barroth', 'JB', 'jb', 'jade barroth']:
+    print ("Jade Barroth is weak to: \n Fire \n and Thunder.")     
+  if monster in ['Duramboros', 'duramboros', 'beyblade']:
+    print ("Duramboros is weak to: \n Fire \n Thunder \n and Ice.")
+  if monster in ['Rust Duramboros', 'rust duramboros', 'rusty']:
+    print ("Rust Duramboros is weak to: \n Fire/Ice \n and Dragon.")     
+  if monster in ['Uragaan', 'uragaan', 'uraghan']:
+    print ("Uragaan is weak to: \n Water \n and Dragon.")     
+  if monster in ['Steel Uragaan', 'steel uragaan', 'steely', 's. uragaan']:
+    print ("Steel Uragaan is weak to: \n Water \n and Dragon.")     
+  if monster in ['Deviljho', 'Joe', 'joe', 'deviljho', 'jho', 'OH SHIT']:
+    print ("Deviljho is weak to: \n Thunder \n Dragon \n Fire \n and Water.")     
+  if monster in ['Brachydios', 'Brachy', 'brachy', 'brachydios', 'dios', 'flagship monster #2', 'explosions']:
+    print ("Brachydios is weak to: \n Water \n and Ice.")     
+  if monster in ['Arzuros', 'shit bear', 'arzuros', 'bear']:
+    print ("Arzuros is weak to... Do you really need help with Arzuros?: \n Fire \n and Ice.")     
+  if monster in ['Lagombi', 'lagombi', 'gombi', 'cute']:
+    print ("Lagombi is weak to: \n Fire \n and Thunder.")     
+  if monster in ['Volvidon', 'Volvi', 'volvi', 'volvidon']:
+    print ("Volvidon is weak to: \n Water \n and Ice.")    
+  if monster in ['Zinogre', 'zinny', 'zinogre', 'lightning guy']:
+    print ("Zinogre is weak to: \n Ice \n and Water/Fire.")  
+  if monster in ['Stygian Zinogre', 'styggy ziggy', 'stygian zinogre', 'bloody mary']:
+    print ("Stygian Zinogre is weak to: \n Thunder \n Ice \n and Water.")    
+  if monster in ['Ceadeus', 'ceadeus', 'gimmick fight #1']:
+    print ("Ceadeus is weak to: \n Dragon \n and Thunder.")    
+  if monster in ['Goldbeard Ceadeus', 'goldbeard ceadeus', 'goldbeard', 'goldilocks']:
+    print ("Goldbeard Ceadeus is weak to: \n Dragon \n and Thunder.")    
+  if monster in ['Jhen Mohran', 'Jhen', 'Mohran', 'jhen mohran', 'boat guy']:
+    print ("Jhen Mohran is weak to: \n Ice/Dragon \n and Water/Thunder.")    
+  if monster in ['Hallowed Jhen Mohran', 'hallowed jhen mohran', 'hallowed', 'hjm']:
+    print ("Hallowed Jhen Mohran is weak to: \n Fire \n and Ice/Dragon.")    
+  if monster in ['', 'No Monster', 'what?', 'lol', 'hi', 'Hello', 'nothing', 'fuck you']:
+    print ("Why are you hunting? Go home. Idiot.")
+    
+
+loop += 1
